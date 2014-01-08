21e3a7a2cedab2d64ff74e08b07a5bfd5f654bb9
Chae Yungi <l8673901@gmail.com>
2014-01-09 00:58:30 +0900

    TouchMoveEx01
    TouchMoveEx01

--- TouchMoveEx01/ViewController.m
+++ TouchMoveEx01/ViewController.m
@@ -8,11 +8,41 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () {
+    BOOL holding;
+}
+@property (weak, nonatomic) IBOutlet UIImageView *image;
 
 @end
 
 @implementation ViewController
+
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
+    UITouch *touch = [touches anyObject];
+    CGPoint point = [touch locationInView:self.view];
+    
+    if(CGRectContainsPoint(self.image.frame, point)) {
+        holding = YES;
+        self.image.transform = CGAffineTransformMakeScale(1.2, 1.2);
+    }
+}
+
+- (void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event {
+    NSLog(@"moved cound:%d",[[event allTouches] count]);
+    
+    if(holding) {
+        UITouch *touch = [touches anyObject];
+        CGPoint point = [touch locationInView:self.view];
+        self.image.center = point;
+    }
+}
+
+- (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event {
+    if(holding) {
+        self.image.transform = CGAffineTransformIdentity;
+        holding = NO;
+    }
+}
 
 - (void)viewDidLoad
 {
